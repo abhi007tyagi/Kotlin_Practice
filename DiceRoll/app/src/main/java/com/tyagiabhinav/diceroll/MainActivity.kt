0e5281bd343c79f39539698d7df00bb594662a29
Abhinav <abhi007tyagi@yahoo.co.in>
2020-10-20 13:56:03 -0500

    Dice Roll Complete!
    Signed-off-by: Abhinav <abhi007tyagi@yahoo.co.in>

--- DiceRoll/app/src/main/java/com/tyagiabhinav/diceroll/MainActivity.kt
+++ DiceRoll/app/src/main/java/com/tyagiabhinav/diceroll/MainActivity.kt
@@ -1,21 +1,36 @@
 package com.tyagiabhinav.diceroll
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+// "late init" because we don't want it to be nullable and check for not null everytime.
+// We are telling Kotlin that we will initialize the variable after we get it in onCreate()
+// var because val is immutable
+lateinit var diceImage: ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        diceImage = findViewById(R.id.diceImage)
         val rollBtn: Button = findViewById(R.id.rollBtn)
-        val rollText: TextView = findViewById(R.id.rollText)
-
         rollBtn.setOnClickListener {
-            Toast.makeText(this, "Yahoo, I rolled", Toast.LENGTH_LONG).show()
+            rollDice()
         }
+    }
+
+    private fun rollDice() {
+        val diceRoll = when ((Math.random() * 6 + 1).toInt()) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> R.drawable.empty_dice
+        }
+        diceImage.setImageResource(diceRoll)
     }
 }
